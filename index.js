38fcf9440b865adf2108e28d0833454fa393ca5d
CKHarrison <hirohumi1882@gmail.com>
2023-06-29 19:27:53 -0700

    finish mvp

--- index.js
+++ index.js
@@ -1,30 +1,85 @@
 const posts = [
-    {
-        name: "Vincent van Gogh",
-        username: "vincey1853",
-        location: "Zundert, Netherlands",
-        avatar: "images/avatar-vangogh.jpg",
-        post: "images/post-vangogh.jpg",
-        comment: "just took a few mushrooms lol",
-        likes: 21
-    },
-    {
-        name: "Gustave Courbet",
-        username: "gus1819",
-        location: "Ornans, France",
-        avatar: "images/avatar-courbet.jpg",
-        post: "images/post-courbet.jpg",
-        comment: "i'm feelin a bit stressed tbh",
-        likes: 4
-    },
-        {
-        name: "Joseph Ducreux",
-        username: "jd1735",
-        location: "Paris, France",
-        avatar: "images/avatar-ducreux.jpg",
-        post: "images/post-ducreux.jpg",
-        comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
-        likes: 152
-    }
-]
+  {
+    name: "Vincent van Gogh",
+    username: "vincey1853",
+    location: "Zundert, Netherlands",
+    avatar: "images/avatar-vangogh.jpg",
+    post: "images/post-vangogh.jpg",
+    comment: "just took a few mushrooms lol",
+    likes: 21,
+  },
+  {
+    name: "Gustave Courbet",
+    username: "gus1819",
+    location: "Ornans, France",
+    avatar: "images/avatar-courbet.jpg",
+    post: "images/post-courbet.jpg",
+    comment: "i'm feelin a bit stressed tbh",
+    likes: 4,
+  },
+  {
+    name: "Joseph Ducreux",
+    username: "jd1735",
+    location: "Paris, France",
+    avatar: "images/avatar-ducreux.jpg",
+    post: "images/post-ducreux.jpg",
+    comment: "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
+    likes: 152,
+  },
+];
 
+const mainEl = document.getElementById("main");
+
+const postSeparatorText = `<div class="post-separator"></div>`;
+
+for (let i = 0; i < posts.length; i++) {
+  let { name, username, location, avatar, post, comment, likes } = posts[i];
+  mainEl.innerHTML += buildPost(name, username, location, avatar, post, comment, likes);
+
+  const likeBtn = document.getElementById("like-btn");
+  likeBtn.addEventListener("dblclick", function () {
+    console.log("button clicked");
+    // likes++;
+    // alert(likes);
+  });
+
+  if (i < posts.length - 1) {
+    mainEl.innerHTML += postSeparatorText;
+  }
+}
+
+function buildPost(name, username, location, avatar, post, comment, likes) {
+  const postText = `
+<div class="container">
+        <section class="post">
+          <div class="post--info">
+            <img src="${avatar}" class="avatar" alt="avatar for ${name}" />
+            <div class="col">
+              <h1 class="post--name">${name}</h1>
+              <p class="post--location">${location}</p>
+            </div>
+          </div>
+
+          <div class="post--content">
+            <img class="post--image" src="${post}" alt="Vincent van Gogh" />
+          </div>
+
+          <div class="col">
+            <div class="post--options">
+              <img class="post--icon like-btn" id="like-btn" src="images/icon-heart.png" alt="heart icon" />
+              <img class="post--icon" src="images/icon-comment.png" alt="heart icon" />
+              <img class="post--icon" src="images/icon-dm.png" alt="heart icon" />
+            </div>
+            <p class="post--likes" >${likes} likes</p>
+          </div>
+
+          <div class="comments col">
+            <p class="comment">
+              <span class="username">${username}</span> <span class="comment">${comment}</span>
+            </p>
+          </div>
+        </section>
+      </div>
+`;
+  return postText;
+}
